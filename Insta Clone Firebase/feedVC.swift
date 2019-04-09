7097f945f236fa810256f1e7d9e5e0bf74107e8e
Peter Jenkin <peternjenkin@gmail.com>
2019-04-09 10:09:30 +0100

    15-122 Tabbed views set up with basic controls

--- Insta Clone Firebase/feedVC.swift
+++ Insta Clone Firebase/feedVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class feedVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Insta Clone Firebase/signInVC.swift
@@ -0,0 +1,42 @@
+//
+//  signInVC.swift
+//  Insta Clone Firebase
+//
+//  Created by Peter Jenkin on 09/04/2019.
+//  Copyright © 2019 Peter Jenkin. All rights reserved.
+//
+
+import UIKit
+
+class signInVC: UIViewController {
+
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func signInBtnClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toTabBar", sender: nil)
+    }
+    @IBAction func signUpBtnClicked(_ sender: Any) {
+    }
+}
