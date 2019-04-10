f50a64b1500cd2033ddb19697a85ab33d44757c4
Peter Jenkin <peternjenkin@gmail.com>
2019-04-10 16:06:35 +0100

    15-129 Uploading files (and listing retrieval url) basically to/fro Firebase

--- Insta Clone Firebase/uploadVC.swift
+++ Insta Clone Firebase/uploadVC.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 import Photos
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
 
 
 class uploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    @IBOutlet weak var postImage: UIImageView!
     
+    @IBOutlet weak var postImage: UIImageView!
     @IBOutlet weak var postComment: UITextView!
+    
+    var uuid = NSUUID().uuidString
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -29,7 +36,59 @@ class uploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     }
 
     @IBAction func postBtnClicked(_ sender: Any) {
+        //let mediaFolder = Storage.storage().reference().child("media")
+        let mediaFolder = Storage.storage().reference().child("media").child("\(uuid).jpg")
+        // folder 'media' previously formed in Storage in Firebase console
+        // 'Storage' as base, 'media' as child
+        // save as randomly generated name (UUID string) each time
+        if let data = UIImageJPEGRepresentation(postImage.image!, 0.5)
+        {
+            guard let userId = Auth.auth().currentUser?.uid else
+            {
+                print ("User not authenticated - no can upload")
+                return
+            }
+                    // https://stackoverflow.com/a/53929727
+            let StorageRef = mediaFolder.child(userId)
+            //mediaFolder.child("\(uuid).jpg").putData(data, metadata: nil) { (metadata, error) in // - deprecated
+            StorageRef.putData(data, metadata: nil) { (metadata, error) in
+                if error != nil
+                {
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                    
+                    let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                    
+                    alert.addAction(okButton)
+                    
+                    self.present(alert, animated: true, completion: nil)
+                    
+                    print("upload path: \(String(describing:metadata?.path))")
+                    
+                }
+                else   // if there's no problem with the file storage, go ahead
+                {
+                    // let imageUrl = metadata?.downloadURL()?.absoluteString   // deprecated
+                    
+                    // downloadUrl must be obtained with async completion/closure, and authentication must be used if the Firebase storage rules are set to same e.g. allow read, write: if request.auth != null; https://stackoverflow.com/a/50464820
+
+                    StorageRef.downloadURL(completion: { (url, error) in
+                        if error != nil
+                        {
+                            print("Error getting download url: \(String(describing: error?.localizedDescription))")
+                        }
+                        else
+                        {
+                            print("url: \(String(describing: url!.absoluteURL))")
+                        }
+                    })
+                    
+                }
+            // putFile(from: // , metadata: <#T##StorageMetadata?#>, completion: <#T##((StorageMetadata?, Error?) -> Void)?##((StorageMetadata?, Error?) -> Void)?##(StorageMetadata?, Error?) -> Void#>)
+            
+            }
+        }
     }
+    
 
     // #selector'd function on image tap
     func choosePhoto()
@@ -64,7 +123,7 @@ class uploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
         
         self.dismiss(animated: true, completion: nil)
     }
-    
 
+        
 }
 
