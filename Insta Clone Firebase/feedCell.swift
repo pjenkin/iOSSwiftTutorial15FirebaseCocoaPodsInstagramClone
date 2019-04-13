683643bc1acf310d07d5e7ace3792e89275e38d2
Peter Jenkin <peternjenkin@gmail.com>
2019-04-13 21:18:50 +0100

    15-134 Layout just about satisfactory, needing improvement

---
+++ Insta Clone Firebase/feedCell.swift
@@ -0,0 +1,13 @@
+//
+//  feedView.swift
+//  Insta Clone Firebase
+//
+//  Created by Peter Jenkin on 10/04/2019.
+//  Copyright © 2019 Peter Jenkin. All rights reserved.
+//
+
+import UIKit
+
+class feedView: UICollectionViewCell {
+    
+}
---
+++ Insta Clone Firebase/feedCellOld.swift
@@ -0,0 +1,25 @@
+//
+//  feedCell.swift
+//  Insta Clone Firebase
+//
+//  Created by Peter Jenkin on 10/04/2019.
+//  Copyright © 2019 Peter Jenkin. All rights reserved.
+//
+
+import UIKit
+
+class feedCellOld: UITableViewCell {
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
